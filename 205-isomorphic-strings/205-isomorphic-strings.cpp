f30e521229d39575a820f885a98869ab23ef4713
Jatin Parashar <78733719+jatinparashar333@users.noreply.github.com>
2022-10-06 09:15:33 +0530

    Time: 6 ms (86.51%), Space: 7.2 MB (12.15%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.size()!=t.size()){
+            return false;
+        }
+        int n=s.size();
+        map<char,char> mp1,mp2;
+        for(int i=0;i<n;i++){
+            if(mp1.find(s[i])==mp1.end() && mp2.find(t[i])==mp2.end()){
+                mp1[s[i]]=t[i];
+                mp2[t[i]]=s[i];
+            }
+            else if(mp1[s[i]]!=t[i] || mp2[t[i]]!=s[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
